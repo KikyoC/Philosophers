886862ae23804d6a78fa8c5ffa50929df34eb9cb
togauthi <togauthier23@gmail.com>
2024-12-19 14:30:53 +0100

    Norm

--- philo/philosophers.c
+++ philo/philosophers.c
@@ -6,20 +6,20 @@
 /*   By: togauthi <togauthi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/18 13:06:30 by togauthi          #+#    #+#             */
-/*   Updated: 2024/12/19 13:14:30 by togauthi         ###   ########.fr       */
+/*   Updated: 2024/12/19 14:29:23 by togauthi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philosophers.h"
 
-int main(int argc, char **argv)
+int	main(int argc, char **argv)
 {
-	t_table table;
+	t_table	table;
 
 	if (argc < 4)
 		return (0);
-	table.die_time = ft_atoi(argv[1]);	
-	table.eat_time = ft_atoi(argv[2]);	
+	table.die_time = ft_atoi(argv[1]);
+	table.eat_time = ft_atoi(argv[2]);
 	table.sleep_time = ft_atoi(argv[3]);
 	printf("%d, %d, %d", table.die_time, table.eat_time, table.sleep_time);
 	return (1);
--- philo/philosophers.h
+++ philo/philosophers.h
@@ -6,36 +6,35 @@
 /*   By: togauthi <togauthi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/18 13:07:16 by togauthi          #+#    #+#             */
-/*   Updated: 2024/12/19 13:14:03 by togauthi         ###   ########.fr       */
+/*   Updated: 2024/12/19 14:30:37 by togauthi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PHILOSOPHERS_H
 # define PHILOSOPHERS_H
-// # include <string.h>
 # include <pthread.h>
 # include <limits.h>
-#include <stdio.h>
+# include <stdio.h>
 
-typedef struct	s_philosopher
+typedef struct s_philosopher
 {
 	struct s_fork	*previous;	
 	struct s_fork	*next;	
 }	t_philosopher;
 
-typedef struct	s_fork
+typedef struct s_fork
 {
 	struct s_philosopher	*previous;	
 	struct s_philosopher	*next;
 }	t_fork;
 
-typedef struct	s_table
+typedef struct s_table
 {
 	struct s_philosopher	*first;
 	int						eat_time;
 	int						sleep_time;
 	int						die_time;
-} 	t_table;
+}	t_table;
 
 int	ft_atoi(const char *nptr);
 
