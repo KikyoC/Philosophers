ee201a85fc071836df3304fcb4b92fdb9d99fd89
Tom GAUTHIER <tom@swordfun.fr>
2025-01-13 11:12:36 +0100

    Eat function is working

--- philo/utils/eat.c
+++ philo/utils/eat.c
@@ -6,25 +6,29 @@
 /*   By: togauthi <togauthi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/06 17:49:48 by togauthi          #+#    #+#             */
-/*   Updated: 2025/01/07 17:57:00 by togauthi         ###   ########.fr       */
+/*   Updated: 2025/01/13 11:12:09 by tom              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../philosophers.h"
+
+unsigned long	get_time(struct timeval tv, struct timeval start)
+{
+	return ((tv.tv_sec * 1000000 + tv.tv_usec / 1000) - (start.tv_sec * 1000000 + start.tv_usec / 1000));
+}
 
 void	*eat(t_philosopher *philosopher, unsigned int time)
 {
 	struct timeval	tv;
 	pthread_mutex_lock(philosopher->left->mutex);
 	gettimeofday(&tv, NULL);
-	ft_print("has taken a fork", tv, philosopher->id, philosopher->table->tv);
+	printf("%lu %i has taken a fork\n", get_time(tv, philosopher->table->tv), philosopher->id);
 	gettimeofday(&tv, NULL);
 	pthread_mutex_lock(philosopher->right->mutex);
-	ft_print("has taken a fork", tv, philosopher->id, philosopher->table->tv);
+	printf("%lu %i has taken a fork\n", get_time(tv, philosopher->table->tv), philosopher->id);
 	gettimeofday(&tv, NULL);
-	ft_print("is eating", tv, philosopher->id, philosopher->table->tv);
-	usleep(100);
-	(void)time;
+	printf("%lu %i is eating\n", get_time(tv, philosopher->table->tv), philosopher->id);
+	usleep(time);
 	pthread_mutex_unlock(philosopher->left->mutex);
 	pthread_mutex_unlock(philosopher->right->mutex);
 	return (NULL);
