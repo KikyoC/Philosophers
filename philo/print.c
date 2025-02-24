489b85f241dc7c6a46f5be7c31b7e5d393638448
togauthi <togauthier23@gmail.com>
2025-02-24 17:00:25 +0100

    A good way to print something with threads

---
+++ philo/print.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   print.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: togauthi <togauthi@42angouleme.fr>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/24 09:40:12 by togauthi          #+#    #+#             */
+/*   Updated: 2025/02/24 13:09:51 by togauthi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philosophers.h"
+
+void	print_message(t_philsopher *philo, char *str)
+{
+	struct timeval	tv;
+	int			time;
+
+	gettimeofday(&tv, NULL);
+	time = (tv.tv_sec * 1000 + tv.tv_usec / 1000) - philo->table->start_time;
+	pthread_mutex_lock(philo->table->print);
+	printf("%i #%i %s\n", time, philo->id, str);
+	pthread_mutex_unlock(philo->table->print);
+}
