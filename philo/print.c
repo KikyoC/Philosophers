aedaec9f7e59e0c003871ee35f62e675fa0485fa
togauthi <togauthier23@gmail.com>
2025-03-10 16:20:33 +0100

    Just to be sure

--- philo/print.c
+++ philo/print.c
@@ -6,7 +6,7 @@
 /*   By: togauthi <togauthi@42angouleme.fr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/24 09:40:12 by togauthi          #+#    #+#             */
-/*   Updated: 2025/02/25 16:43:06 by togauthi         ###   ########.fr       */
+/*   Updated: 2025/03/10 16:19:45 by togauthi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,8 @@ void	print_message(t_philosopher *philo, char *str)
 	int				time;
 
 	gettimeofday(&tv, NULL);
+	if (is_died(philo->table))
+		return ;
 	time = (tv.tv_sec * 1000 + tv.tv_usec / 1000) - philo->table->start_time;
 	pthread_mutex_lock(philo->table->print);
 	printf("%i #%i %s\n", time, philo->id, str);
